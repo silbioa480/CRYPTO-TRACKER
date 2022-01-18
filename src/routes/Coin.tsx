342867fb76a7f9cfbfbcce429f1fe0b01673c1c4
silbioa480 <silbioa480@gmail.com>
2022-01-18 23:31:44 +0900

    add chart

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -287,7 +287,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
