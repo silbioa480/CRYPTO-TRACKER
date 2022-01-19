617bbafeda594918e8541faf6f58907bedc28c2d
silbioa480 <silbioa480@gmail.com>
2022-01-19 22:56:28 +0900

    #4.14

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -54,6 +54,16 @@ function Chart({ coinId }: ChartProps) {
               labels: { show: false },
               axisTicks: { show: false },
               axisBorder: { show: false },
+              type: "datetime",
+              categories: data?.map((price) => price.time_close),
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: { formatter: (value) => `${value.toFixed(2)}` },
             },
           }}
         ></ApexChart>
